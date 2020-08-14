7ba2fb274ae4e34de19256a9ad18e1a44412552d
Denys Don <ddelacruz@gumindanao.edu.ph>
2020-08-15 01:00:42 +0800

    Integrate to Firebase
    SHOP DATA:
        - shop.data deleted
        - transfer all shop data to firebase
        - fetch on redux
    
    Features:
        - Added Spinner on Loading
        - Fetch Data on Shop
        - Added Shop Collection on Click

---
+++ src/components/withSpinner/withSpinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './withSpinner.style.scss';
+
+const WithSpinner = (WrappedComponent) => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+      <div className='spinnerOverlay'>
+        <div className='spinnerContainer' />
+      </div>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    );
+  };
+  return Spinner;
+};
+
+export default WithSpinner;
